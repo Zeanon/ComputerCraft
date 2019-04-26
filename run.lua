5ee6fa5472a175bfffc087a203a3f14d02ecf3d2
Zeanon <thezeanon@gmail.com>
2019-04-26 22:14:06 +0200

    Update

--- run.lua
+++ run.lua
@@ -1,4 +1,4 @@
-print("Running DraconicReactor")
+print("Running Draconic Reactor Control Program")
 while true do
   if multishell.getTitle(1) ~= "Draconic Reactor" and multishell.getTitle(2) ~= "Draconic Reactor" and multishell.getTitle(3) ~= "Draconic Reactor" then
     newTabID = shell.openTab("DraconicReactor")
