1e5ec13ebde61f5c0d2f57f3f58e29cdc3fff3d1
Zeanon <thezeanon@gmail.com>
2019-04-26 21:06:17 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,13 +1,2 @@
-while true do
-  if multishell.getCount() < 3 then
-    if multishell.getTitle(1) ~= "Draconic Reactor" and multishell.getTitle(2) ~= "Draconic Reactor" and multishell.getTitle(3) ~= "Draconic Reactor" then
-      newTabID = shell.openTab("DraconicReactor")
-      multishell.setTitle(newTabID, "Draconic Reactor")
-    end
-    if multishell.getTitle(1) ~= "Config" and multishell.getTitle(2) ~= "Config" and multishell.getTitle(3) ~= "Config" then
-	    newTabID = shell.openTab("clear")
-      multishell.setTitle(newTabID, "Config")
-    end
-  end 
-  os.sleep(5);
-end
+newTabID = shell.openTab("run")
+multishell.setTitle(newTabID, "Run")
