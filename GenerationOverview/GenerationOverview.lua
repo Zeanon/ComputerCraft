d5d4dbd0e15dc3185c0368f816339050e1316d2c
Zeanon <thezeanon@gmail.com>
2019-04-30 00:32:49 +0200

    Update

--- GenerationOverview/GenerationOverview.lua
+++ GenerationOverview/GenerationOverview.lua
@@ -57,37 +57,37 @@ function update()
         print("Total reactor output: " .. output)
         a = gui.getInteger(output / 1000000)
         if a ~= 0 then
-            gui.draw_number(output, 1000000, mon, 4, 4, color)
+            gui.draw_number(output, 1000000, mon, 5, 4, color)
         end
         output = output - (1000000 * gui.getInteger(output / 1000000))
         b = gui.getInteger(output / 100000)
         if a ~= 0 or b ~= 0 then
-            gui.draw_number(output, 100000, mon, 8, 4, color)
+            gui.draw_number(output, 100000, mon, 9, 4, color)
         end
         output = output - (100000 * gui.getInteger(output / 100000))
         c = gui.getInteger(output / 10000)
         if a ~= 0 or b ~= 0 or c ~= 0 then
-            gui.draw_number(output, 10000, mon, 12, 4, color)
+            gui.draw_number(output, 10000, mon, 13, 4, color)
         end
         output = output - (10000 * gui.getInteger(output / 10000))
         d = gui.getInteger(output / 1000)
         if a ~= 0 or b ~= 0 or c ~= 0 or d ~= 0 then
-            gui.draw_number(output, 1000, mon, 16, 4, color)
+            gui.draw_number(output, 1000, mon, 17, 4, color)
         end
         output = output - (1000 * gui.getInteger(output / 1000))
         e = gui.getInteger(output / 100)
         if a ~= 0 or b ~= 0 or c ~= 0 or d ~= 0 or e ~= 0 then
-            gui.draw_number(output, 100, mon, 20, 4, color)
+            gui.draw_number(output, 100, mon, 21, 4, color)
         end
         output = output - (100 * gui.getInteger(output / 100))
         f = gui.getInteger(output / 10)
         if a ~= 0 or b ~= 0 or c ~= 0 or d ~= 0 or e ~= 0 or f ~= 0 then
-            gui.draw_number(output, 10, mon, 24, 4, color)
+            gui.draw_number(output, 10, mon, 25, 4, color)
         end
         output = output - (10 * gui.getInteger(output / 10))
-        gui.draw_number(output, 1, mon, 28, 4, color)
+        gui.draw_number(output, 1, mon, 29, 4, color)
 
-        drawRFT(mon, 33, 4, rftcolor)
+        drawRFT(mon, 34, 4, rftcolor)
     end
     sleep(0.5)
 end
