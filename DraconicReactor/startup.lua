100848a541eaffc5b4e4c76a8fc64847cae6433a
Zeanon <thezeanon@gmail.com>
2019-04-30 03:11:04 +0200

    Update

--- DraconicReactor/startup.lua
+++ DraconicReactor/startup.lua
@@ -1,2 +1,3 @@
 local newTabID = shell.openTab("run")
 multishell.setTitle(newTabID, "Draconic Reactor")
+multishell.setFocus(newTabID)
--- GenerationOverview/startup.lua
+++ GenerationOverview/startup.lua
@@ -1,2 +1,3 @@
 local newTabID = shell.openTab("run")
 multishell.setTitle(newTabID, "Generation Overview")
+multishell.setFocus(newTabID)
