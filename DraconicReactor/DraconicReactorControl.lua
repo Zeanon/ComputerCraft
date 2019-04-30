f240036791e9ea3d2e5719532458337647632aef
Zeanon <thezeanon@gmail.com>
2019-05-01 01:12:30 +0200

    Update

--- DraconicReactor/DraconicReactorControl.lua
+++ DraconicReactor/DraconicReactorControl.lua
@@ -383,6 +383,10 @@ function buttons()
             if curInputGate == math.huge or isnan(curInputGate) or curInputGate < 0 then
                 curInputGate = 0
             end
+
+            if curInputGate > maxOutput then
+                curInputGate = maxOutput
+            end
             inputfluxgate.setSignalLowFlow(curInputGate)
             save_config()
         end
