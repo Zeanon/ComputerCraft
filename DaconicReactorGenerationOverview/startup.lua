c6b3c5ea8899adc6a0d90668812c59f5b8d3ead5
Zeanon <thezeanon@gmail.com>
2019-05-03 15:15:14 +0200

    Update

--- DaconicReactorGenerationOverview/startup.lua
+++ DaconicReactorGenerationOverview/startup.lua
@@ -1,4 +1,4 @@
 local newTabID = shell.openTab("run")
-multishell.setTitle(newTabID, "Generation Overview")
+multishell.setTitle(newTabID, "Dracnonic Reactor Generation Overview")
 sleep(0.01)
 multishell.setFocus(newTabID)
