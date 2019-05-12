21169502c5d44286b79a8dc41bfd174e1760d560
Zeanon <thezeanon@gmail.com>
2019-05-12 22:52:15 +0200

    Update update.lua

--- EnergyCoreOverview/update.lua
+++ EnergyCoreOverview/update.lua
@@ -1,1 +1,9 @@
+-- Updater for DraconicReactorControl by drmon and Zeanon
+-- get it with pastebin get UEi3KkwM update
+-- pastebin link: https://pastebin.com/UEi3KkwM
 
+if fs.exists("install") then
+    shell.run("delete install")
+end
+shell.run("pastebin get UKxFmqXx install")
+shell.run("install")
