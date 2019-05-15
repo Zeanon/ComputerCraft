9c2eb9c8ac1df13f1afbfc252c0b31c71940e795
Zeanon <thezeanon@gmail.com>
2019-05-15 16:34:48 +0200

    Update EnergyCoreOverview.lua

--- EnergyCoreOverview/EnergyCoreOverview.lua
+++ EnergyCoreOverview/EnergyCoreOverview.lua
@@ -484,16 +484,10 @@ function drawLine(mon, localY, line, drawButtons, side)
 		end
 	elseif line == 5 then
 		local flow = (totalEnergy - oldEnergy) / (20 * refresh)
-		if flow < 0 then
-			flow = flow * (-1)
-		end
 		local length = string.len(tostring(flow))
 		local offset = (length * 4) + (2 * gui.getInteger((length - 1) / 3)) + 9
 		local x = ((mon.X - offset) / 2) - 1
-		if totalEnergy - oldEnergy < 0 then
-			x = ((mon.X - offset - 4) / 2) - 1
-			gui.draw_line(mon, x, localY + 2, 3, numberColor)
-		end
+		
 		gui.draw_number(mon, flow, x + 9, localY, numberColor)
 		gui.draw_rft(mon, x, localY, unitColor)
 		if drawButtons then
