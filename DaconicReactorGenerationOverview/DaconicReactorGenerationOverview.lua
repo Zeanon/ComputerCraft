5eb1ae9756a33da465baf59f035eb443256640ad
Zeanon <thezeanon@gmail.com>
2019-05-17 02:40:16 +0200

    Update

--- DaconicReactorGenerationOverview/DaconicReactorGenerationOverview.lua
+++ DaconicReactorGenerationOverview/DaconicReactorGenerationOverview.lua
@@ -209,13 +209,13 @@ function drawLines()
 		generation = getGeneration()
 		drainback = getDrainback()
 		gui.clear(mon)
-		print("Total reactor output: " .. gui.format_int(generation - drainback) .. "RF/t")
-		print("Total generation: " .. gui.format_int(generation) .. "RF/t")
+		print("Total reactor output: " .. gui.format_int(generation - drainback) .. " RF/t")
+		print("Total generation: " .. gui.format_int(generation) .. " RF/t")
 		for i = 1, reactorCount do
 			reactorGeneration[i] = getReactorGeneration(i)
-			print("Reactor " .. i .. " Generation: " .. gui.format_int(reactorGeneration[i]) .. "RF/t")
+			print("Reactor " .. i .. " Generation: " .. gui.format_int(reactorGeneration[i]) .. " RF/t")
 		end
-		print("Total drainback: " .. gui.format_int(drainback) .. "RF/t")
+		print("Total drainback: " .. gui.format_int(drainback) .. " RF/t")
 
 		local amount = monitors[connectedMonitors[i] .. ":amount"]
 		local drawButtons = monitors[connectedMonitors[i] .. ":drawButtons"]
