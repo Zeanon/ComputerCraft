5e6bf445fef744c3beb762848ccb00b46643dce6
Zeanon <thezeanon@gmail.com>
2020-04-07 03:07:58 +0200

    Update DraconicReactorControl.lua

--- DraconicReactorControl/DraconicReactorControl.lua
+++ DraconicReactorControl/DraconicReactorControl.lua
@@ -749,14 +749,14 @@ function update()
         gui.draw_line(mon, 0, 10, mon.X + 1, colors.gray)
         gui.draw_column(mon, mon.X - 25, 1, mon.Y, colors.gray)
 
-        gui.draw_text_lr(mon, 2, 12, 26, "Energy Saturation  T:" .. targetSat, satPercent .. "%", colors.white, satColor, colors.black)
+        gui.draw_text_lr(mon, 2, 12, 26, "Energy Saturation  T:" .. targetSat .. "%", satPercent .. "%", colors.white, satColor, colors.black)
         gui.progress_bar(mon, 2, 13, mon.X - 28, satPercent, 100, satColor, colors.lightGray)
 
         gui.draw_text_lr(mon, 2, 15, 26, "Temperature  M:" .. maxTemperature .. "C", gui.format_int(ri.temperature) .. "C", colors.white, tempColor, colors.black)
         gui.progress_bar(mon, 2, 16, mon.X - 28, ri.temperature, maxTemperature, tempColor, colors.lightGray)
 
         if autoInputGate then
-            gui.draw_text_lr(mon, 2, 18, 26, "Field Strength  T:" .. targetStrength, fieldPercent .. "%", colors.white, fieldColor, colors.black)
+            gui.draw_text_lr(mon, 2, 18, 26, "Field Strength  T:" .. targetStrength .. "%", fieldPercent .. "%", colors.white, fieldColor, colors.black)
         else
             gui.draw_text_lr(mon, 2, 18, 26, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
         end
