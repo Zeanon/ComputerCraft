95a0da54ba57fd561259d7f8cd664a27b546bc10
Zeanon <thezeanon@gmail.com>
2022-04-24 12:08:59 +0200

    Updated variable names

--- DraconicReactorGenerationOverview/install.lua
+++ DraconicReactorGenerationOverview/install.lua
@@ -1,31 +1,31 @@
 -- Installer for GenerationOverview by Zeanon
 -- get it with pastebin get VT6ezUgB install
 -- pastebin link: https://pastebin.com/VT6ezUgB
-local libURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/gui.lua"
-local lib2URL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/color.lua"
+local guiLibURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/gui.lua"
+local colorLibURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/color.lua"
 local startupURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorGenerationOverview/startup.lua"
 local runURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorGenerationOverview/run.lua"
 local generationOverviewURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorGenerationOverview/DraconicReactorGenerationOverview.lua"
-local lib, lib2, startup, run, generationOverview
-local libFile, lib2File, startupFile, runFile, generationOverviewFile
+local guiLib, colorLib, startup, run, exe
+local guiLibFile, colorLibFile, startupFile, runFile, exeFile
 
 
 fs.makeDir("lib")
 
-lib = http.get(libURL)
+guiLib = http.get(guiLibURL)
 
-libFile = fs.open("lib/gui", "w")
-libFile.write(lib.readAll())
-libFile.close()
-lib.close()
+guiLibFile = fs.open("lib/gui", "w")
+guiLibFile.write(guiLib.readAll())
+guiLibFile.close()
+guiLib.close()
 
 
-lib2 = http.get(lib2URL)
+colorLib = http.get(colorLibURL)
 
-lib2File = fs.open("lib/color", "w")
-lib2File.write(lib2.readAll())
-lib2File.close()
-lib2.close()
+colorLibFile = fs.open("lib/color", "w")
+colorLibFile.write(colorLib.readAll())
+colorLibFile.close()
+colorLib.close()
 
 
 startup = http.get(startupURL)
@@ -44,12 +44,12 @@ runFile.close()
 run.close()
 
 
-generationOverview = http.get(generationOverviewURL)
+exe = http.get(generationOverviewURL)
 
-generationOverviewFile = fs.open("DraconicReactorGenerationOverview", "w")
-generationOverviewFile.write(generationOverview.readAll())
-generationOverviewFile.close()
-generationOverview.close()
+exeFile = fs.open("DraconicReactorGenerationOverview", "w")
+exeFile.write(exe.readAll())
+exeFile.close()
+exe.close()
 
 
 if fs.exists("update") then
