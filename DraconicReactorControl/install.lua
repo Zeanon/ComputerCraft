bc0b64e10954fb9a489b0f1f820fdff9bbbd5739
Zeanon <thezeanon@gmail.com>
2022-04-24 12:05:31 +0200

    updated variable names

--- DraconicReactorControl/install.lua
+++ DraconicReactorControl/install.lua
@@ -2,22 +2,22 @@
 -- get it with pastebin get UKxFmqXx install
 -- pastebin link: https://pastebin.com/UKxFmqXx
 
-local libURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/gui.lua"
+local guiLibURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/lib/gui.lua"
 local startupURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorControl/startup.lua"
 local runURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorControl/run.lua"
 local reactorControlURL = "https://raw.githubusercontent.com/Zeanon/ComputerCraft/master/DraconicReactorControl/DraconicReactorControl.lua"
-local lib, startup, run, reactorControl
-local libFile, startupFile, runFile, reactorControlFile
+local guiLib, startup, run, exe
+local guiLibFile, startupFile, runFile, exeFile
 
 
 fs.makeDir("lib")
 
-lib = http.get(libURL)
+guiLib = http.get(guiLibURL)
 
-libFile = fs.open("lib/gui", "w")
-libFile.write(lib.readAll())
-libFile.close()
-lib.close()
+guiLibFile = fs.open("lib/gui", "w")
+guiLibFile.write(guiLib.readAll())
+guiLibFile.close()
+guiLib.close()
 
 
 startup = http.get(startupURL)
@@ -36,12 +36,12 @@ runFile.close()
 run.close()
 
 
-reactorControl = http.get(reactorControlURL)
+exe = http.get(reactorControlURL)
 
-reactorControlFile = fs.open("DraconicReactor", "w")
-reactorControlFile.write(reactorControl.readAll())
-reactorControlFile.close()
-reactorControl.close()
+exeFile = fs.open("DraconicReactor", "w")
+exeFile.write(exe.readAll())
+exeFile.close()
+exe.close()
 
 
 if fs.exists("update") then
