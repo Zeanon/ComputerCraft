c0181b93291028715c46332f99fffcea36747cef
Zeanon <thezeanon@gmail.com>
2022-05-03 11:14:30 +0200

    Create update.lua

---
+++ DraconicReactorControl_legacy/update.lua
@@ -0,0 +1,10 @@
+-- Updater for DraconicReactorControl by acidjazz and Zeanon
+-- get it with pastebin get DEPNPsSz update
+-- pastebin link: https://pastebin.com/DEPNPsSz
+
+if fs.exists("install") then
+    shell.run("delete install")
+end
+
+shell.run("pastebin get qpA31HT6 install")
+shell.run("install")
