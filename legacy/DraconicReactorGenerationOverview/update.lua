f7e067bfe7136e94a7b5c1778be59a741bb3f1f0
Zeanon <thezeanon@gmail.com>
2022-05-03 11:39:48 +0200

    Create update.lua

---
+++ legacy/DraconicReactorGenerationOverview/update.lua
@@ -0,0 +1,8 @@
+-- Updater for GenerationOverview by Zeanon
+-- get it with pastebin get HZ7ffzMn update
+-- pastebin link: https://pastebin.com/HZ7ffzMn
+if fs.exists("install") then
+    shell.run("delete install")
+end
+shell.run("pastebin get VT6ezUgB install")
+shell.run("install")
